a1c51730b2cf4322b149c9a39671af33a24f79ac
ItielSanz /AXO <113627289+ItielSanzAXO@users.noreply.github.com>
2023-05-31 00:44:50 -0600

    update

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@
 import React from "react";
 import { render } from "react-dom";
 
-import App from "./App.jsx";
+import App from "src/App.jsx";
 
 render(<App></App>, document.getElementById("app"));
